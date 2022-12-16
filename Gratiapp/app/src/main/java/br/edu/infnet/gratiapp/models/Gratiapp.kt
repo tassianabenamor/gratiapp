910a1e341d21f7db931d26726908557d4b390291
Tassiana Benamor <tassiana.benamor@gmail.com>
2022-12-16 13:30:27 -0300

    Delete Gratiapp directory

--- Gratiapp/app/src/main/java/br/edu/infnet/gratiapp/models/Gratiapp.kt
+++
@@ -1,8 +0,0 @@
-package br.edu.infnet.gratiapp.models
-
-data class Gratiapp(
-    val nomeGratiapp: String = "",
-    val data: String = "",
-    val humor: String = "",
-    val agradecer: String = ""
-)
--- Gratiapp/app/src/main/java/br/edu/infnet/gratiapp/models/GratiappWithId.kt
+++
@@ -1,9 +0,0 @@
-package br.edu.infnet.gratiapp.models
-
-data class GratiappWithId(
-    val nomeGratiapp: String = "",
-    val data: String = "",
-    val humor: String = "",
-    val agradecer: String = "",
-    var id: String = ""
-)
